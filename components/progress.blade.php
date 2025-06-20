f6a5f718d2d04d2f4cbbfd83f63be1479a7c57b1
TJ <tj@jiannius.com>
2025-06-20 18:08:20 +0800

    fix: showing null%

--- components/progress.blade.php
+++ components/progress.blade.php
@@ -146,7 +146,7 @@ x-init="$nextTick(() => $dispatch('get-job-tracker'))">
                 <template x-if="isRunning && tracker.progress < 100" hidden>
                     <div class="flex items-center gap-2">
                         <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="text-zinc-500 size-5" fill="currentColor"><style>.spinner_ajPY{transform-origin:center;animation:spinner_AtaB .75s infinite linear}@keyframes spinner_AtaB{100%{transform:rotate(360deg)}}</style><path d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z" opacity=".25"/><path d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z" class="spinner_ajPY"/></svg>
-                        <span x-text="`${tracker.progress}%`"></span>
+                        <span x-text="`${tracker.progress || 0}%`"></span>
                     </div>
                 </template>
 
@@ -166,13 +166,13 @@ x-init="$nextTick(() => $dispatch('get-job-tracker'))">
 
                 <div class="w-full rounded-full h-3 bg-zinc-100 overflow-hidden">
                     <div
-                    x-bind:style="`width: ${isQueued ? 0 : tracker.progress}%`"
+                    x-bind:style="`width: ${isQueued ? 0 : (tracker.progress || 0)}%`"
                     x-bind:class="{
                         'bg-green-500': tracker.progress >= 100,
                         'bg-red-500': hasErrors,
-                        'bg-accent': tracker.progress < 100 && empty(tracker.errors),
+                        'bg-sky-600': tracker.progress < 100 && empty(tracker.errors),
                     }"
-                    class="h-full bg-accent transition-all duration-500"></div>
+                    class="h-full transition-all duration-500"></div>
                 </div>
 
                 <div class="flex items-center gap-2">
